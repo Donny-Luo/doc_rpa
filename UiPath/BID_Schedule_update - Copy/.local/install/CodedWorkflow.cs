bb1677383b087be72a254551f8e9bb8238a06900
Donny Luo <johnkaylou@hotmail.com>
2023-10-11 10:26:52 +0800

    Add new

---
+++ UiPath/BID_Schedule_update - Copy/.local/install/CodedWorkflow.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+
+namespace BID_Schedule_update
+{
+    public class CodedWorkflow : CodedWorkflowBase
+    {
+        public CodedWorkflow()
+        {
+            _ = new System.Type[]{};
+        }
+    }
+}
